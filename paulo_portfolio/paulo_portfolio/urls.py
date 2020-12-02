81212929aa2d33225adf62294ade18dcdd270037
Olo Panganiban <olopangs@gmail.com>
2020-12-02 09:05:33 +0800

    Heroku Deployment

--- paulo_portfolio/paulo_portfolio/urls.py
+++ paulo_portfolio/paulo_portfolio/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
     path('hello_world/', include('hello_world.urls')),
     path('projects/', include('projects.urls')),
     path('blog/', include('blog.urls')),
